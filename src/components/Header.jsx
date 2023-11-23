5d9b4ec10601cb6340bbad05b57f9102352e8a13
liamonk <61438758+liamonk@users.noreply.github.com>
2023-11-23 14:28:39 +1300

    added image to header (placeholder for now)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,22 @@ const StyledHeaderMain = styled.div`
   padding: 10px;
   margin-bottom: 20px;
   box-shadow: 5px 5px lightgrey;
+  display: flex;
 `;
 
 export default function header() {
   return (
     <StyledHeaderMain>
       <h1>Pi and Seek</h1>
+      <img
+        style={{
+          height: "70px",
+          marginLeft: "auto",
+          marginTop: "auto",
+          marginBottom: "auto",
+        }}
+        src="src/assets/logo.png"
+      />
     </StyledHeaderMain>
   );
 }
